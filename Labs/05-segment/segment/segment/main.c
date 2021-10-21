f4b27fd78491ba507d47b2c3c261b4b6b17378ba
FilipNim <73553092+FilipNim@users.noreply.github.com>
2021-10-21 10:41:43 +0200

    Add files via upload

---
+++ Labs/05-segment/segment/segment/main.c
@@ -0,0 +1,58 @@
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include "segment.h"        // Seven-segment display library for AVR-GCC
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Display decimal counter values on SSD (Seven-segment 
+ *           display) when 16-bit Timer/Counter1 overflows.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Configure SSD signals
+    SEG_init();
+
+    // Test of SSD: display number '3' at position 0
+    
+
+    // Configure 16-bit Timer/Counter1 for Decimal counter
+    // Set the overflow prescaler to 262 ms and enable interrupt
+    TIM1_overflow_262ms();
+    TIM1_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Increment decimal counter value and display it on SSD.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{       
+    static uint8_t i = 0;
+    static uint8_t pos = 0;
+     if (i<10)
+     {
+         SEG_update_shift_regs(i, pos);
+         i++;
+     }
+     else {
+         i = 0;
+} 
+}
